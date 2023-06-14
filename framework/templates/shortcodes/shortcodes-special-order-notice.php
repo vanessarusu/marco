dfa3385026e5259a0f68840a0cee6b5ae4d03a12
Vanessa Rusu <vanessa@Vanessas-MacBook-Pro-2.local>
2023-06-14 16:12:59 -0700

    updating template

--- framework/templates/shortcodes/shortcodes-special-order-notice.php
+++ framework/templates/shortcodes/shortcodes-special-order-notice.php
@@ -1,25 +1,44 @@
-<?php
-global $current_user;
+<?php if( is_cart() || is_checkout() ){
 
-$params = array(
-	'theme_location' => 'bar',
-	'menu' => '2023 Top Bar Menu'
-);
+$is_special_order = false;
 
-//get_currentuserinfo();
-?>
-<ul id="userNav">
-<?php if ( is_user_logged_in() ) : ?>
-    <li>
-        <a href="<?php bloginfo('url'); ?>/dealer" target="blank"><span class="header-contact-text">Dealer Dashboard</span></a>
-    </li>
-    <li>
-        <a href="<?php echo wp_logout_url(); ?>"><span class="header-contact-text">Logout</span></a>
-    </li>
+// check each cart item for our category
+foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 
-<?php else: ?>
-    <li><a href="<?php bloginfo('url'); ?>/wp-admin" target="blank"><span class="header-contact-text">Dealer &amp; Architect Login</span></a></li>
-<?php endif; ?>
-<?php wp_nav_menu($params); ?>
+    $product = $cart_item['data'];
 
-</ul>
+    if ( has_term( 'special-order', 'product_cat', $product->id ) ) {
+        $is_special_order = true;
+        break;
+    }
+}
+
+// if has special order products, show notice
+if ( $is_special_order ) { ?>
+
+    <section class="section-so-notice">
+        <div class="row">
+            <div class="col span-12">
+                <div class="wc-cart-so-notice">
+
+                    <h1 class="required-red">Order Warning</h1>
+
+                    <h4>Your order contains products which require freight shipping and cannot be quoted accurately online.</h4>
+
+                    <p>On the cart page, click "Calculate Shipping", then select one of the following options:</p>
+
+                    <ul>
+                        <li>Select <b>"Local Pickup"</b> if you prefer picking up your order at our Bright, Ontario location. You may pay for your order online.</li>
+                        <li>Select <b>"Ground Delivery"</b> if you require delivery. Proceed with the checkout and use the <i>Offline Payment</i> option. After your order is placed, a Mar-Co Sales Rep will contact you to finalize delivery costs and payment.</li>
+                    </ul>
+
+                    <p>You may <a href="<?php bloginfo('url'); ?>/contact/">contact Mar-Co Clay</a> for assistance.</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php }
+
+}

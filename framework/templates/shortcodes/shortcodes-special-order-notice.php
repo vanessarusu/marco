6cce5b80579e29e15d9cd727548044c96ada82b3
Vanessa Rusu <vanessa@Vanessas-MacBook-Pro-2.local>
2023-06-14 17:12:12 -0700

    cart page banner

--- framework/templates/shortcodes/shortcodes-special-order-notice.php
+++ framework/templates/shortcodes/shortcodes-special-order-notice.php
@@ -1,4 +1,4 @@
-<?php if( is_cart() || is_checkout() ){
+<?php if( is_cart() || is_checkout() ) {
 
 $is_special_order = false;
 
@@ -42,3 +42,18 @@ if ( $is_special_order ) { ?>
     <?php }
 
 }
+		// Special Order Message
+		$terms = wp_get_post_terms( $post->ID, 'product_cat' );
+		foreach ( $terms as $term ) $categories[] = $term->slug;
+
+		if ( in_array( 'special-order', $categories ) ) {
+		?>
+
+			<div class="special-order-box">
+
+				<h3>Special Order Product</h3>
+				<p><strong>The shipping costs for this product cannot be accurately quoted online.</strong> You can still add this product to your cart and submit your order online; simply follow the instructions on the <a href="<?php bloginfo('url'); ?>/cart/">Cart</a> and Checkout pages.</p>
+
+			</div>
+
+		<?php	}	
